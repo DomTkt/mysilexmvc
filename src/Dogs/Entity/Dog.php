9dfb87de852110a3ecee6b1bc96d55ee58384029
LAURENT MATHIS p1406269 <mathis.laurent@etu.univ-lyon1.fr>
2016-10-07 16:39:40 +0200

    new module dog

--- src/Dogs/Entity/Dog.php
+++ src/Dogs/Entity/Dog.php
@@ -1,8 +1,58 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace App\Dogs\Entity;
 
+class Dog
+{
+    protected $id;
+
+    protected $nom;
+
+    protected $maitre;
+    
+    public function __construct($id, $nom, $maitre)
+    {
+        $this->id = $id;
+        $this->nom = $nom;
+        $this->maitre = $maitre;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    public function setMaitre($maitre)
+    {
+        $this->maitre = $maitre;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+   
+    public function getNom()
+    {
+        return $this->nom;
+    }
+    public function getMaitre()
+    {
+        return $this->maitre;
+    }
+
+    public function toArray()
+    {
+        $array = array();
+        $array['id'] = $this->id;
+        $array['nom'] = $this->nom;
+        $array['maitre'] = $this->maitre;
+
+        return $array;
+    }
+}
---
+++ src/Dogs/Repository/DogRepository.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Users\Repository;
+
+use App\Dogs\Entity\Dog;
+use Doctrine\DBAL\Connection;
+
+/**
+ * User repository.
+ */
+class UserRepository
+{
+    /**
+     * @var \Doctrine\DBAL\Connection
+     */
+    protected $db;
+
+    public function __construct(Connection $db)
+    {
+        $this->db = $db;
+    }
+
+   /**
+    * Returns a collection of users.
+    *
+    * @param int $limit
+    *   The number of users to return.
+    * @param int $offset
+    *   The number of users to skip.
+    * @param array $orderBy
+    *   Optionally, the order by info, in the $column => $direction format.
+    *
+    * @return array A collection of users, keyed by user id.
+    */
+   public function getAll()
+   {
+       $queryBuilder = $this->db->createQueryBuilder();
+       $queryBuilder
+           ->select('d.*')
+           ->from('dogs', 'd');
+
+       $statement = $queryBuilder->execute();
+       $dogsData = $statement->fetchAll();
+       foreach ($dogsData as $dogData) {
+           $dogEntityList[$dogData['id']] = new Dog($dogData['id'], $dogData['nom'], $dogData['maitre']);
+       }
+
+       return $dogEntityList;
+   }
+
+   /**
+    * Returns an User object.
+    *
+    * @param $id
+    *   The id of the user to return.
+    *
+    * @return array A collection of users, keyed by user id.
+    */
+   public function getById($id)
+   {
+       $queryBuilder = $this->db->createQueryBuilder();
+       $queryBuilder
+           ->select('d.*')
+           ->from('dogs', 'd')
+           ->where('id = ?')
+           ->setParameter(0, $id);
+       $statement = $queryBuilder->execute();
+       $dogData = $statement->fetchAll();
+
+       return new Dog($dogData[0]['id'], $dogData[0]['nom'], $dogData[0]['maitre']);
+   }
+
+    public function delete($id)
+    {
+        $queryBuilder = $this->db->createQueryBuilder();
+        $queryBuilder
+          ->delete('dogs')
+          ->where('id = :id')
+          ->setParameter(':id', $id);
+
+        $statement = $queryBuilder->execute();
+    }
+
+    public function update($parameters)
+    {
+        $queryBuilder = $this->db->createQueryBuilder();
+        $queryBuilder
+          ->update('dogs')
+          ->where('id = :id')
+          ->setParameter(':id', $parameters['id']);
+
+        if ($parameters['nom']) {
+            $queryBuilder
+              ->set('nom', ':nom')
+              ->setParameter(':nom', $parameters['nom']);
+        }
+        
+        if ($parameters['maitre']) {
+            $queryBuilder
+            ->set('maitre', ':maitre')
+            ->setParameter(':maitre', $parameters['maitre']);
+        }
+
+        $statement = $queryBuilder->execute();
+    }
+
+    public function insert($parameters)
+    {
+        $queryBuilder = $this->db->createQueryBuilder();
+        $queryBuilder
+          ->insert('dogs')
+          ->values(
+              array(
+                'nom' => ':nom',
+                'maitre' => ':maitre',
+              )
+          )
+          ->setParameter(':nom', $parameters['nom'])
+          ->setParameter(':maitre', $parameters['maitre']);
+        $statement = $queryBuilder->execute();
+    }
+}
+
